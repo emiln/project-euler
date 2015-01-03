2ede6969ad7030976573babfcf1aa7e2417914cf
emiln <emil@nauerby.com>
2015-01-03 13:58:45 +0100

    Better algorithm for problem 1
    This new algorithm actually uses math instead of generating three sequences
    of numbers. This should make it potentially useful for later, bigger challenges.

--- src/euler/problem1.clj
+++ src/euler/problem1.clj
@@ -1,6 +1,16 @@
 (ns euler.problem1)
 
-(->> (concat (range 0 1000 3) (range 0 1000 5))
-  (apply hash-set)
-  (reduce +)
-  println)
+(defn sum-multiples
+  "Returns the sum of i*k for 0 <= i*k < n."
+  [n k]
+  (let [r (mod n k)
+        n' (if (zero? r)
+             n
+             (+ n (- k r)))]
+    (/ (* n' (- n' k))
+       (* 2 k))))
+
+(-> (sum-multiples 1000 3)
+    (+ (sum-multiples 1000 5))
+    (- (sum-multiples 1000 15))
+    println)
